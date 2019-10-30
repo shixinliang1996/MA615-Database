c82f9807e25c6c2ab2ed145810aabb82ccc0ddbd
sxliang <sxliang@bu.edu>
2019-10-29 21:26:00 -0400

    4NF

--- ShixinLiang.R
+++ ShixinLiang.R
@@ -6,16 +6,47 @@ JFC <- read_xlsx("Top MA Donors 2016-2020.xlsx", sheet = "JFC Contributions (DO 
 
 ## Remove NA
 #contrib_all <- na.omit(contrib_all)
-#View(contrib_all)
+View(contrib_all)
 
+
+# Second Normal Form
 contributiors <- select(contrib_all, contribid, fam, contrib, City, State, Zip, Fecoccemp, orgname)
-View(contributiors)
-
 orgs <- select(contrib_all, orgname, ultorg)
-View(orgs)
+contribution <- select(contrib_all, fectransid, cycle, contribid, date, amount, type, recipient)
+recipients <- select(contrib_all, recipient, recipid, recipcode, cmteid)
 
-contribution <- select(contrib_all, contribid, date, amount, recipient)
-View(contribution)
+# Third Normal Form
+contributiors <- select(contrib_all, contribid, fam, contrib, Zip, Fecoccemp, orgname)
+zipcode <- select(contrib_all, Zip, City)
+city <- select(contrib_all, City, State)
+orgs <- select(contrib_all, orgname, ultorg)
+contribution <- select(contrib_all, fectransid, contribid, date, amount, type, recipient)
+dates <- select(contrib_all, date, cycle)
+recipients <- select(contrib_all, recipient, recipid, recipcode, cmteid)
 
-recipients <- select(contrib_all, recipient, party, recipcode, cmteid)
-View(recipients)
+# Fourth Normal Form
+contributiors <- select(contrib_all, contribid, fam, contrib)
+employer <- select(contrib_all, contribid, Fecoccemp)
+organization <- select(contrib_all, contribid, orgname)
+zipcode <- select(contrib_all, contribid, Zip)
+city <- select(contrib_all, Zip, City)
+state <- select(contrib_all, City, State)
+orgs <- select(contrib_all, orgname, ultorg)
+contribution <- select(contrib_all, fectransid, contribid, date, amount, type, recipient)
+dates <- select(contrib_all, date, cycle)
+recipients <- select(contrib_all, recipient, recipid, recipcode, cmteid)
+
+# Eliminate duplicative rowa
+contributiors <- base::unique(contributiors) 
+employer <- base::unique(employer) 
+organization <- base::unique(organization) 
+zipcode <- base::unique(zipcode) 
+city <- base::unique(city) 
+state <- base::unique(state) 
+orgs <- base::unique(orgs) 
+contribution <- base::unique(contribution) 
+dates <- base::unique(dates) 
+recipients <- base::unique(recipients) 
+
+
+
